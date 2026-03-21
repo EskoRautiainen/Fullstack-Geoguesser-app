764618d90eaee33c198f1f05cb1142c5d8dd037b
EskoRautiainen <Rautiainen98@gmail.com>
2026-03-21 15:48:45 +0200

    feat(connection.js): Create separate file for connecting to SQLite in-memory db

---
+++ connection.js
@@ -0,0 +1,23 @@
+// Define connection to database
+const sqlite3 = require('sqlite3');
+// open is required to open in memory database
+const { open } = require('sqlite');
+
+async function createConnection() {
+try {
+  // Open an in-memory database
+  const db = await open({
+    filename: ':memory:',
+    driver: sqlite3.Database,
+  });
+
+  console.log('Connected to SQLite in-memory database!');
+  return db;
+} catch (err) {
+    console.error('Connection failed:', err.message);
+    // Throw err crashes createConnection immediately. Without throw err, createConnection will return undefined.
+    throw err;
+}
+};
+
+module.exports = createConnection;
